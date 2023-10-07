15c8526a34d78c7820a2a25297f23168257012de
Granite Bagas <granitebagas28@gmail.com>
2023-10-07 23:46:20 +0700

    Add summary module and separate report module

---
+++ src/report/report.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+  ParseUUIDPipe,
+  ParseEnumPipe,
+} from '@nestjs/common';
+import { ReportType } from 'src/data';
+import { ReportService } from './report.service';
+import {
+  CreateReportDto,
+  ReportResponseDto,
+  UpdateReportDto,
+} from '../dtos/report.dto';
+
+@Controller('report/:type')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Get()
+  getAllReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+  ): ReportResponseDto[] {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.getAllReports(reportType);
+  }
+
+  @Get(':id')
+  getReportById(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.getReportById(reportType, id);
+  }
+
+  @Post()
+  createReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Body() { amount, source }: CreateReportDto,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.createReport(reportType, { amount, source });
+  }
+
+  @Put(':id')
+  updateReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdateReportDto,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.updateReport(reportType, id, body);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id', ParseUUIDPipe) id: string) {
+    return this.reportService.deleteReport(id);
+  }
+}
